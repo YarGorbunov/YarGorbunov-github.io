447e820cdb49e2d475713f9476431e5294503ec9
unknown <yaroslav-gorbunov99@mail.ru>
2022-10-09 12:35:57 +0300

    sam v shoke

--- task5/script.js
+++ task5/script.js
@@ -4,7 +4,8 @@ function calc() {
     let amount=document.getElementById("amount").value;
     let price=document.getElementById("price").value;
     let result=document.getElementById("result");
-    if (amount=="" || price=="") {
+    /*
+	if (amount=="" || price=="") {
         result.innerHTML = "Не пойдёт";
         result.style.color="#dc3545";
     }
@@ -16,4 +17,12 @@ function calc() {
         result.innerHTML = "Не пойдёт";
         result.style.color="#dc3545";
     }
+	*/
+	if (amount*price!="" && !isNaN(amount*price)){
+		result.innerHTML = amount*price;
+        result.style.color="#2718dc";
+	} else {
+		result.innerHTML = "Не пойдёт";
+        result.style.color="#dc3545";
+	}	
 }
