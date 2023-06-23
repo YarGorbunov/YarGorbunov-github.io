0cd57b4519af7ae3901069ad9ca59b4d3ae38a5b
yargorbunov <yaroslav-gorbunov99@mail.ru>
2023-06-23 13:59:14 +0300

    .

--- testvkapp/script.js
+++ testvkapp/script.js
@@ -1,5 +1,5 @@
 document.getElementById("send").addEventListener("click", function () {
     let xhr = new XMLHttpRequest();
-    xhr.open("GET","https://oauth.vk.com/authorize?client_id=51687171&display=page&redirect_uri=http://example.com/callback&response_type=code&v=5.131");
+    xhr.open("GET","https://oauth.vk.com/authorize?client_id=51687171&display=page&redirect_uri=https://yargorbunov.github.io/testvkapp/&response_type=code&v=5.131");
     xhr.send();
 });
